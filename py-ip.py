855e23036efd4aa23b24f5858a9f51e2ed708542
BigRush <tom.harpaz98@gmail.com>
2018-12-13 20:37:10 +0200

    work in progress

--- py-ip.py
+++ py-ip.py
@@ -13,8 +13,29 @@
 
 import os
 import socket
+import ifaddr
 
-int_ip = socket.gethostbyname(socket.gethostname())
+def Internal_IP():
+	try:
+		int_ip = socket.gethostbyname(socket.gethostname())
+		print("Your internal IP is: {}".format(int_ip))
+	
+	except:
+		print("Can't retrieve internal IP")
+		
+
+def External_IP():
+	inter = ifaddr.get_adapters()
+	for i in inter:
+            for j in i.ips:
+                #print(filter)
+                #print(type(filter))
+                pre_filter = "{}: {}".format(i.nice_name, j.ip)
+                filter += "{}\n".format(pre_filter)
+
+        print(filter)
+        result = re.match(r'enp*', filter)
+        print(result.group(0))
 
 
 
