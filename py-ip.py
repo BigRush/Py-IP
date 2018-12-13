ab732c5bc90ed05215a4c1558a6e6afe54d5eab7
BigRush <tom.harpaz98@gmail.com>
2018-12-13 16:20:19 +0200

    initial commit

---
+++ py-ip.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+
+################################################################################
+# Author :	BigRush
+#
+# License :  GPLv3
+#
+# Description :  Shows internal and external IP address
+#
+# Version :  1.0.0
+################################################################################
+
+
+import os
+import socket
+
+int_ip = socket.gethostbyname(socket.gethostname())
+
+
+
